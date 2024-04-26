99076587aed38b24e3a492bdd4a61bfaab23a797
Midnight0110 <165168416+Midnight0110@users.noreply.github.com>
2024-04-26 16:19:17 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function myFunction(){
+
+var element = document.body;
+element.classList.toggle("dark-mode");
+
+}
+
+
+
+let mybutton = document.getElementById("myButn");
+window.onscroll = function() {
+    scrollFunction()
+};
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        mybutton.style.display = "block";
+    } else{
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
